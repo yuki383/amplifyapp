c5b82838c8f6c2eaaf20a1812832704debcd045e
yuu383 <yuukikokubo.pg@gmail.com>
2021-12-27 18:39:49 +0900

    added auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuthenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css'
 
-function App() {
+function App(props) {
+  const hoge = useAuthenticator();
+  console.log({ props, hoge });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
+        <div>Hello {hoge.user.username}</div>
+      <div>
+        <h2>SignOut</h2>
+        <button onClick={hoge.signOut}>Sign Out</button>
+      </div>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
